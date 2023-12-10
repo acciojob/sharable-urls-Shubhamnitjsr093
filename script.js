f89b9299fc3ba47e04f1f6f8d9eca787163f068f
Shubhamnitjsr093 <99527655+Shubhamnitjsr093@users.noreply.github.com>
2023-12-10 20:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+function updateUrl() {
+            // Get input values
+            var name = document.getElementById('name').value;
+            var year = document.getElementById('year').value;
+
+            // Construct the query string
+            var queryString = "?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+
+            // Update the h3 element with the new URL
+            document.getElementById('url').innerText = "https://localhost:8080/" + queryString;
+        }
